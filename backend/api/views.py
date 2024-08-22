a81a0f347085180f5b4ac5a351d8d4084f8c34cc
mihir <mihir.pande22@pccoepune.org>
2024-08-22 12:45:53 +0530

    echo get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,12 @@
 from django.http import JsonResponse
-
+import json
 
 def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data.keys())
     return JsonResponse({'message': 'Hello, World!'})
